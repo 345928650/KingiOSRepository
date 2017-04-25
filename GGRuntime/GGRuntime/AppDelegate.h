14233fe7aca30775939ac811934e6704cbe53945
king w <kingw@kingdemacbook-pro.local>
2017-04-25 18:27:34 +0800

    提交工程初始文件

---
+++ GGRuntime/GGRuntime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGRuntime
+//
+//  Created by king w on 2017/4/25.
+//  Copyright © 2017年 king. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
